ea2a8736bd72a36b0fc37742060f5f815b21a96e
Mary Brady <emceebrady@gmail.com>
2018-08-03 15:36:46 -0600

    kick and punch buttons; update parameters

--- game.js
+++ game.js
@@ -1,13 +1,33 @@
 
-
 let health = 100;
+let hits = 0;
+let name = 'Name Here'
 
 function update() {
-    document.getElementById("health").innerHTML = health;
+    if(health < 0) {
+        return
+    }
+  document.getElementById("health").innerHTML = health;
+  document.getElementById("hits").innerHTML = hits;
+  document.getElementById("name").innerHTML = name;
 }
 
 function slap() {
-    health -= 1
+    health -= 1;
+    hits += 1;
     update()
 }
+
+function kick() {
+    health -= 10;
+    hits += 1;
+    update()
+}
+
+function punch() {
+    health -= 5;
+    hits += 1;
+    update()
+}
+
 update();
